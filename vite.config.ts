9498d0faccc68cba92b8a0d9b1c446f3b5e9adf2
im3too8 <43242390+im3too8@users.noreply.github.com>
2025-03-09 19:50:40 +0000

    Fixed syntax errors in vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,8 @@ import { componentTagger } from "lovable-tagger";
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
   base: "/alshareef-apps-hub/",
+  build: {
+    outDir: "dist",},
   server: {
     host: "::",
     port: 8080,
